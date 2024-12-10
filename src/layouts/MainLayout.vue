8f44ab43de3d6e2e7b7dc1bec732139602274699
iranika109 <7370486+iranika@users.noreply.github.com>
2024-12-11 01:42:16 +0900

    add: input fields

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -12,23 +12,21 @@
         />
 
         <q-toolbar-title>
-          Quasar App
+          BZCF PROTOTYPE
         </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
 
     <q-drawer
       v-model="leftDrawerOpen"
-      show-if-above
       bordered
     >
       <q-list>
         <q-item-label
           header
         >
-          Essential Links
+          各種リンク
         </q-item-label>
 
         <EssentialLink
@@ -61,36 +59,6 @@ const linksList = [
     caption: 'github.com/quasarframework',
     icon: 'code',
     link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
   }
 ]
 
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,13 +1,41 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page class="flex q-pa-md justify-center">
+    <div class="row">
+      <div>
+        <q-input class="col q-mx-sm" :v-model="submit.name" label="名前" ></q-input>
+        <q-input class="col q-mx-sm" :v-model="submit.hurigana" label="ふりがな"></q-input>
+        <q-select class="col q-mx-sm" :v-model="submit.職業" :options="options" label="職業" />
+        <q-file
+          v-model="files"
+          label="Pick files"
+          filled
+          counter
+          :counter-label="counterLabelFn"
+          max-files="3"
+          multiple
+          style="max-width: 300px"
+        >
+          <template v-slot:prepend>
+            <q-icon name="attach_file" />
+          </template>
+        </q-file>
+
+      </div>
+      <q-btn label="Submit" type="submit" color="primary"/>
+    </div>
+
   </q-page>
 </template>
 
 <script setup>
 //
+import {ref} from "vue"
+
+let submit = {
+  name : ref(""),
+  hurigana : ref(""),
+  職業 : ref()
+}
+let options = ["会社員","学生"]
+
 </script>
